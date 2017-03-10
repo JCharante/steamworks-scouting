c3dfd86692f8e36e73291aa9d4874ec78cca00a8
John <jcharante@gmail.com>
2017-03-09 23:38:03 -0500

    Finished adding entry fields

--- app/www/scout_match/script.js
+++ app/www/scout_match/script.js
@@ -13,7 +13,20 @@ function onceDocumentReady() {
 				auto_hopper: false,
 				auto_collect: false,
 				auto_gear_pos: 'none',
-				auto_high_goal: 'none'
+				auto_high_goal_pos: 'none',
+				climb_rating: 'neutral',
+				gear_rating: 'neutral',
+				total_gears: 0,
+				gear_dispense_method: 'none',
+				got_gear_from_human: false,
+				got_gear_from_floor: false,
+				high_goal_rating: 'ds',
+				high_goal_shoot_from_key: false,
+				high_goal_shoot_from_wall: false,
+				high_goal_shoot_from_afar: false,
+				low_goal_rating: 'ds',
+				total_hoppers: 0,
+				collected_from_hopper: false
 			}
 		}
 	})
