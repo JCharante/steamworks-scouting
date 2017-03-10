074652a3d5b7e0e4d0e4244e2f769cfb45781bfd
John <jcharante@gmail.com>
2017-03-09 23:03:27 -0500

    Added a form to the scout match page with some auto elements

---
+++ app/www/scout_match/script.js
@@ -0,0 +1,20 @@
+function onceDocumentReady() {
+	var scout_match = new Vue({
+		el: '#vue-app',
+		methods: {
+		},
+		data: function() {
+			return {
+				event_name: 'practice',
+				team_number: 5687,
+				match_number: 13,
+				auto_line_cross: false,
+				auto_low_goal: false,
+				auto_gear_pos: 'none',
+				auto_high_goal: 'none'
+			}
+		}
+	})
+}
+
+$(document).ready(onceDocumentReady);
