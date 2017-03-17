5bcb6e2911e72b0fb29b5dffb76c7c278e1692b2
John <jcharante@gmail.com>
2017-03-17 01:57:05 -0400

    Added page to upload match data

--- app/www/static/internal/js/navbar_component.js
+++ app/www/static/internal/js/navbar_component.js
@@ -20,6 +20,7 @@ Vue.component('achilles-navbar', {
 					'<li><a href="../scout_match/index.html">Scout Match</a></li>' +
 					'<li><a href="../scoutedMatches/index.html">Scouted Matches</a></li>' +
 					'<li><a href="../scanMatch/index.html">Scan Match</a></li>' +
+					'<li><a href="../uploadToServer/index.html">Upload Data</a></li>' +
 					'<li class="dropdown">' +
 					'<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Developer <span class="caret"></span></a>' +
 						'<ul class="dropdown-menu">' +
---
+++ app/www/uploadToServer/script.js
@@ -0,0 +1,40 @@
+function onceDocumentReady() {
+	var upload_page = new Vue({
+		el: '#vue-app',
+		methods: {
+			upload: function () {
+				var matches = JSON.parse(localStorage.getItem('matches') || '{}');
+				var data = {
+					'matches': []
+				};
+
+				for (var key in matches) {
+					if(!matches.hasOwnProperty(key)) continue;
+
+					var match = matches[key];
+					data.matches.push(match)
+				}
+				console.log(data);
+				$.ajax({
+					method: 'POST',
+					url: 'http://achilles.jcharante.com:9876/match/upload',
+					data: JSON.stringify(data),
+					dataType: "json",
+					contentType: "application/json",
+					statusCode: {
+						200: function (data) {
+							console.log('Server Replied: ', data);
+						},
+						400: function (responseObject) {
+							console.log('Server Replied: ', responseObject);
+							var data = responseObject.responseJSON;
+						}
+					}
+				});
+			}
+		}
+	});
+}
+
+
+$(document).ready(onceDocumentReady);
