57f17c41842eaa16bb6bb7cec0de5ee12719bdd9
John <git@jcharante.com>
2017-03-30 03:02:10 -0400

    Created MatchV4 schema with auto_kpa, total_kpa, and collected_fuel_from floor. Moved migration functions to migration.py. Updated functions to work with new schema.

---
+++ server/migration.py
@@ -0,0 +1,119 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_setup import Base, MatchV1, MatchV2, MatchV3, MatchV4
+import util
+import uuid
+from typing import Tuple, List, Dict
+import exceptions
+import os
+from datetime import datetime
+import dateutil.parser
+import re
+
+
+# Connects to the database
+engine = create_engine(os.environ['db_address'])
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+
+def migrate_from_matchv1_to_matchv2():
+	session = DBSession()
+	migrated_matches = []  # type: List[str]
+	for match in session.query(MatchV1).all():  # type: MatchV1
+		session.add(MatchV2(
+			match_id=match.match_id, event_name=match.event_name, team_number=match.team_number,
+			match_number=int(re.sub("[^0-9]", "", match.match_number)),
+			auto_line_cross=match.auto_line_cross, auto_low_goal=match.auto_low_goal, auto_hopper=match.auto_hopper,
+			auto_collect=match.auto_collect, auto_gear_pos=match.auto_gear_pos, auto_high_goal_pos=match.auto_high_goal_pos,
+			climb_rating=match.climb_rating, gear_rating=match.gear_rating, total_gears=match.total_gears,
+			gear_dispense_method=match.gear_dispense_method, got_gear_from_human=match.got_gear_from_human,
+			got_gear_from_floor=match.got_gear_from_floor, high_goal_rating=match.high_goal_rating,
+			high_goal_shoot_from_key=match.high_goal_shoot_from_key, high_goal_shoot_from_wall=match.high_goal_shoot_from_wall,
+			high_goal_shoot_from_afar=match.high_goal_shoot_from_afar, low_goal_rating=match.low_goal_rating,
+			total_hoppers=match.total_hoppers, collected_from_hopper=match.collected_from_hopper, last_modified=match.last_modified
+		))
+		migrated_matches.append(match.match_id)
+	for migrated_match_id in migrated_matches:
+		session.query(MatchV1).filter(MatchV1.match_id == migrated_match_id).delete()
+	session.commit()
+	session.close()
+
+
+def migrate_matchv2_to_matchv3():
+	session = DBSession()
+	migrated_matches = []  # type: List[str]
+	for match in session.query(MatchV2).all():  # type: MatchV2
+		session.add(MatchV3(
+			match_id=match.match_id,
+			event_name=match.event_name,
+			team_number=match.team_number,
+			match_number=match.match_number,
+			auto_line_cross=match.auto_line_cross,
+			auto_low_goal=match.auto_low_goal,
+			auto_hopper=match.auto_hopper,
+			auto_collect=match.auto_collect,
+			auto_gear_pos=match.auto_gear_pos,
+			auto_high_goal_pos=match.auto_high_goal_pos,
+			climb_rating=match.climb_rating,
+			gear_rating=match.gear_rating,
+			total_gears=match.total_gears,
+			gear_dispense_method=match.gear_dispense_method,
+			got_gear_from_human=match.got_gear_from_human,
+			got_gear_from_floor=match.got_gear_from_floor,
+			high_goal_rating=match.high_goal_rating,
+			high_goal_shoot_from_key=match.high_goal_shoot_from_key,
+			high_goal_shoot_from_wall=match.high_goal_shoot_from_wall,
+			high_goal_shoot_from_afar=match.high_goal_shoot_from_afar,
+			low_goal_rating=match.low_goal_rating,
+			total_hoppers=match.total_hoppers,
+			collected_from_hopper=match.collected_from_hopper,
+			last_modified=match.last_modified,
+			notes=''
+		))
+		migrated_matches.append(match.match_id)
+	for migrated_match_id in migrated_matches:
+		session.query(MatchV2).filter(MatchV2.match_id == migrated_match_id).delete()
+	session.commit()
+	session.close()
+
+
+def migrate_matchv3_to_matchv4():
+	session = DBSession()
+	migrated_matches = []  # type: List[str]
+	for match in session.query(MatchV3).all():  # type: MatchV3
+		session.add(MatchV4(
+			match_id=match.match_id,
+			event_name=match.event_name,
+			team_number=match.team_number,
+			match_number=match.match_number,
+			auto_line_cross=match.auto_line_cross,
+			auto_low_goal=match.auto_low_goal,
+			auto_hopper=match.auto_hopper,
+			auto_collect=match.auto_collect,
+			auto_gear_pos=match.auto_gear_pos,
+			auto_high_goal_pos=match.auto_high_goal_pos,
+			auto_kpa=0,
+			climb_rating=match.climb_rating,
+			gear_rating=match.gear_rating,
+			total_gears=match.total_gears,
+			total_kpa=0,
+			gear_dispense_method=match.gear_dispense_method,
+			got_gear_from_human=match.got_gear_from_human,
+			got_gear_from_floor=match.got_gear_from_floor,
+			high_goal_rating=match.high_goal_rating,
+			high_goal_shoot_from_key=match.high_goal_shoot_from_key,
+			high_goal_shoot_from_wall=match.high_goal_shoot_from_wall,
+			high_goal_shoot_from_afar=match.high_goal_shoot_from_afar,
+			low_goal_rating=match.low_goal_rating,
+			total_hoppers=match.total_hoppers,
+			collected_from_hopper=match.collected_from_hopper,
+			collected_fuel_from_floor=False,
+			last_modified=match.last_modified,
+			notes=''
+		))
+		migrated_matches.append(match.match_id)
+	for migrated_match_id in migrated_matches:
+		session.query(MatchV3).filter(MatchV3.match_id == migrated_match_id).delete()
+	session.commit()
+	session.close()
--- server/server.py
+++ server/server.py
@@ -5,9 +5,11 @@ import os
 from typing import List, Dict
 import db_functions
 import exceptions
+import migration
 
-db_functions.migrate_from_matchv1_to_matchv2()
-db_functions.migrate_matchv2_to_matchv3()
+migration.migrate_from_matchv1_to_matchv2()
+migration.migrate_matchv2_to_matchv3()
+migration.migrate_matchv3_to_matchv4()
 app = Flask(__name__)
 
 
@@ -88,10 +90,12 @@ def api_match_upload():
 		auto_hopper = match.get('auto_hopper', None)
 		auto_collect = match.get('auto_collect', None)
 		auto_gear_pos = match.get('auto_gear_pos', None)
+		auto_kpa = match.get('auto_kpa', None)
 		auto_high_goal_pos = match.get('auto_high_goal_pos', None)
 		climb_rating = match.get('climb_rating', None)
 		gear_rating = match.get('gear_rating', None)
 		total_gears = match.get('total_gears', None)
+		total_kpa = match.get('total_kpa', None)
 		gear_dispense_method = match.get('gear_dispense_method', None)
 		got_gear_from_human = match.get('got_gear_from_human', None)
 		got_gear_from_floor = match.get('got_gear_from_floor', None)
@@ -102,6 +106,7 @@ def api_match_upload():
 		low_goal_rating = match.get('low_goal_rating', None)
 		total_hoppers = match.get('total_hoppers', None)
 		collected_from_hopper = match.get('collected_from_hopper', None)
+		collected_fuel_from_floor = match.get('collected_fuel_from_floor', None)
 		last_modified = match.get('last_modified', None)
 		notes = match.get('notes', None)
 
@@ -123,6 +128,8 @@ def api_match_upload():
 			return http_400(1, 'Invalid Data Type', 'auto_collect')
 		if type(auto_gear_pos) is not str:
 			return http_400(1, 'Invalid Data Type', 'auto_gear_pos')
+		if type(auto_kpa) is not int:
+			return http_400(1, 'Invalid Data Type', 'auto_kpa')
 		if type(auto_high_goal_pos) is not str:
 			return http_400(1, 'Invalid Data Type', 'auto_high_goal_pos')
 		if type(climb_rating) is not str:
@@ -131,6 +138,8 @@ def api_match_upload():
 			return http_400(1, 'Invalid Data Type', 'gear_rating')
 		if type(total_gears) is not int:
 			return http_400(1, 'Invalid Data Type', 'total_gears')
+		if type(total_kpa) is not int:
+			return http_400(1, 'Invalid Data Type', 'total_kpa')
 		if type(gear_dispense_method) is not str:
 			return http_400(1, 'Invalid Data Type', 'gear_dispense_method')
 		if type(got_gear_from_human) is not bool:
@@ -151,37 +160,42 @@ def api_match_upload():
 			return http_400(1, 'Invalid Data Type', 'total_hoppers')
 		if type(collected_from_hopper) is not bool:
 			return http_400(1, 'Invalid Data Type', 'collected_from_hopper')
+		if type(collected_fuel_from_floor) is not bool:
+			return http_400(1, 'Invalid Data Type', 'collected_fuel_from_floor')
 		if type(last_modified) is not str:
 			return http_400(1, 'Invalid Data Type', 'last_modified')
 		if type(notes) is not str:
 			return http_400(1, 'Invalid Data Type', 'notes')
 
 		try:
-			db_functions.add_matchv3(match_id,
-									 event_name,
-									 team_number,
-									 match_number,
-									 auto_line_cross,
-									 auto_low_goal,
-									 auto_hopper,
-									 auto_collect,
-									 auto_gear_pos,
-									 auto_high_goal_pos,
-									 climb_rating,
-									 gear_rating,
-									 total_gears,
-									 gear_dispense_method,
-									 got_gear_from_human,
-									 got_gear_from_floor,
-									 high_goal_rating,
-									 high_goal_shoot_from_key,
-									 high_goal_shoot_from_wall,
-									 high_goal_shoot_from_afar,
-									 low_goal_rating,
-									 total_hoppers,
-									 collected_from_hopper,
-									 last_modified,
-									 notes)
+			db_functions.add_matchv4(match_id=match_id,
+									 event_name=event_name,
+									 team_number=team_number,
+									 match_number=match_number,
+									 auto_line_cross=auto_line_cross,
+									 auto_low_goal=auto_low_goal,
+									 auto_hopper=auto_hopper,
+									 auto_collect=auto_collect,
+									 auto_gear_pos=auto_gear_pos,
+									 auto_kpa=auto_kpa,
+									 auto_high_goal_pos=auto_high_goal_pos,
+									 climb_rating=climb_rating,
+									 gear_rating=gear_rating,
+									 total_gears=total_gears,
+									 total_kpa=total_kpa,
+									 gear_dispense_method=gear_dispense_method,
+									 got_gear_from_human=got_gear_from_human,
+									 got_gear_from_floor=got_gear_from_floor,
+									 high_goal_rating=high_goal_rating,
+									 high_goal_shoot_from_key=high_goal_shoot_from_key,
+									 high_goal_shoot_from_wall=high_goal_shoot_from_wall,
+									 high_goal_shoot_from_afar=high_goal_shoot_from_afar,
+									 low_goal_rating=low_goal_rating,
+									 total_hoppers=total_hoppers,
+									 collected_from_hopper=collected_from_hopper,
+									 collected_fuel_from_floor=collected_fuel_from_floor,
+									 last_modified=last_modified,
+									 notes=notes)
 		except exceptions.MatchDataOutdated:
 			pass
 
