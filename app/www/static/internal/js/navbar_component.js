22e97afba639b8998678126b93650bb966228859
John <git@jcharante.com>
2017-03-30 01:56:57 -0400

    Made the brand in the navbar a link to the home page

--- app/www/static/internal/js/navbar_component.js
+++ app/www/static/internal/js/navbar_component.js
@@ -10,7 +10,7 @@ Vue.component('achilles-navbar', {
 					'<span class="icon-bar"></span>' +
 					'<span class="icon-bar"></span>' +
 				'</button>' +
-				'<a class="navbar-brand" href="">achilles</a>' +
+				'<a class="navbar-brand" href="../home/index.html">achilles</a>' +
 			'</div>' +
 
 			'<!-- Collect the nav links, forms, and other content for toggling -->' +
