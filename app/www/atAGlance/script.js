f3e24b0ddfcdbfda2c7d2c1b56e9a8d5acc1f3a1
John <jcharante@gmail.com>
2017-03-31 05:42:31 -0400

    At a glance now shows avg kpa (auto/total), avg gears, and floor pickup for fuel & gears. All calculated for the current event and from locally stored matches.

--- app/www/atAGlance/script.js
+++ app/www/atAGlance/script.js
@@ -64,47 +64,79 @@ Vue.component('match', {
 });
 
 
-Vue.component('matches', {
-	props: ['team_number'],
-	template:
-	'<div class="row">' +
-		'<match v-for="match in matches" :match="match" v-on:load-scouted-matches="loadScoutedMatches()"></match>' +
-	'</div>',
-	watch: {
-		team_number: function(val, oldVal){
-			var self = this;
-			self.matches = [];
-
-			var all_matches = JSON.parse(localStorage.getItem('matches') || '{}');
-
-			for (var key in all_matches) {
-				if(!all_matches.hasOwnProperty(key)) continue;
-
-				var match = all_matches[key];
-
-				if (match.team_number === self.team_number) {
-					self.matches.push(match);
-				}
-			}
-		}
-	},
-	data: function() {
-		return {
-			matches: []
-		}
-	}
-});
-
-
 function onceDocumentReady() {
 	var app = new Vue({
 		el: '#vue-app',
 		mounted: function() {
 			var self = this;
 		},
+		methods: {
+			loadMatches: function() {
+				var self = this;
+				self.matches = [];
+
+				var all_matches = JSON.parse(localStorage.getItem('matches') || '{}');
+
+				for (var key in all_matches) {
+					if(!all_matches.hasOwnProperty(key)) continue;
+
+					var match = all_matches[key];
+
+					if (match.team_number === self.team_number) {
+						self.matches.push(match);
+					}
+				}
+			},
+			calculateStats: function() {
+
+			}
+		},
+		watch: {
+			team_number: function(val, oldVal){
+				var self = this;
+				self.loadMatches();
+				self.calculateStats();
+
+				var currentEvent = 'pine-tree';  // TODO: Make this an option in the settings page.
+
+				var totalAutokPa = 0;
+				var totalTotalkPa = 0;
+				var totalGears = 0;
+				var matches = 0;
+
+				for (var i = 0; i < self.matches.length; i++) {
+					var match = self.matches[i];
+
+					if (match.event_name === currentEvent) {
+						matches += 1;
+						totalAutokPa += match.auto_kpa;
+						totalTotalkPa += match.total_kpa;
+						totalGears += match.total_gears;
+
+						if (match.collected_fuel_from_floor) {
+							self.can_pickup_fuel_from_floor = true;
+						}
+
+						if (match.got_gear_from_floor) {
+							self.can_pickup_gears_from_floor = true;
+						}
+					}
+				}
+
+				self.averageAutokPa = (totalAutokPa / matches) || 0;
+				self.averageTotalkPa = (totalTotalkPa / matches) || 0;
+				self.averageGears = (totalGears / matches) || 0;
+			}
+		},
 		data: function () {
 			return {
-				team_number: null
+				team_number: null,
+				matches: [],
+				can_pickup_fuel_from_floor: false,
+				can_pickup_gears_from_floor: false,
+				averageAutokPa: 0,
+				averageTotalkPa: 0,
+				averageGears: 0
 			}
 		}
 	})
