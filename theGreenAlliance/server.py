43b58f9d2b20e5f669a7d50f30ebc3ffb626282c
JCharante <git@jcharante.com>
2017-04-26 00:18:00 -0400

    Added twisted klein test

--- theGreenAlliance/server.py
+++ theGreenAlliance/server.py
@@ -1,1 +1,18 @@
+from twisted.web.static import File
+from klein import Klein
 import db_functions
+
+app = Klein()
+
+
+@app.route('/')
+def pg_index(request):
+	return 'The Green Alliance'
+
+
+@app.route('/static', branch=True)
+def pg_static(request):
+	return File('./static/')
+
+
+app.run('0.0.0.0', 8080)
