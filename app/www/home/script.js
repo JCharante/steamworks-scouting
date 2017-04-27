fbb56694f3a538b96d91aae2402f650e4fba819e
JCharante <git@jcharante.com>
2017-04-27 02:32:54 -0500

    Converted the alert at the home screen to a toast

--- app/www/home/script.js
+++ app/www/home/script.js
@@ -1,7 +1,25 @@
 function onceDocumentReady() {
 	if ((localStorage.getItem('scoutName') || '') === '') {
-		alert('Please set your scout name in the settings page.');
-		window.location.replace('../settings/index.html');
+		/*
+		setTimeout is here because while cordova.js loads before this script does,
+		cordova_plugins.js does not, and therefor the toast feature isn't available yet.
+		 */
+		setTimeout(function() {
+			window.plugins.toast.showWithOptions({
+				message: 'Set your scout name under the settings page',
+				duration: 'short',
+				position: 'bottom',
+				styling: {
+					backgroundColor: toastColors.error
+				}
+			}, function(result) {
+				if (result && result.event) {
+					if (result.event === 'hide' || result.event === 'touch') {
+						window.location.replace('../settings/index.html');
+			        }
+				}
+			});
+		}, 1000);
 	}
 }
 
