6e6bbca43184b694635e152fdf379f9d108aeb6f
JCharante <git@jcharante.com>
2017-04-27 03:03:50 -0500

    Added schema & migration function for MatchV6

--- server/db_setup.py
+++ server/db_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine
+from sqlalchemy import Column, Integer, String, Float, Boolean, create_engine, Text
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
@@ -159,6 +159,40 @@ class MatchV5(Base):
 	last_modified = Column(String(30))
 	notes = Column(String(200))
 	scout_name = Column(String(30))
+
+
+class MatchV6(Base):
+	__tablename__ = 'MatchV6'
+	pk = Column(Integer, primary_key=True)
+	match_id = Column(String(36))
+	event_name = Column(Text())
+	team_number = Column(Integer)
+	match_number = Column(Integer)
+	auto_line_cross = Column(Boolean)
+	auto_low_goal = Column(Boolean)
+	auto_hopper = Column(Boolean)
+	auto_collect = Column(Boolean)
+	auto_gear_pos = Column(Text())
+	auto_high_goal_pos = Column(Text())
+	auto_kpa = Column(Integer)
+	climb_rating = Column(Text())
+	gear_rating = Column(Text())
+	total_gears = Column(Integer)
+	total_kpa = Column(Integer)
+	gear_dispense_method = Column(Text())
+	got_gear_from_human = Column(Boolean)
+	got_gear_from_floor = Column(Boolean)
+	high_goal_rating = Column(Text())
+	high_goal_shoot_from_key = Column(Boolean)
+	high_goal_shoot_from_wall = Column(Boolean)
+	high_goal_shoot_from_afar = Column(Boolean)
+	low_goal_rating = Column(Text())
+	total_hoppers = Column(Integer)
+	collected_from_hopper = Column(Boolean)
+	collected_fuel_from_floor = Column(Boolean)
+	last_modified = Column(Text())
+	notes = Column(Text())
+	scout_name = Column(Text())
 
 
 engine = create_engine(os.environ['db_address'])
