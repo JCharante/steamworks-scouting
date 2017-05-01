6bea5c8331131f25c2296f47f0d1507e12d19f2a
JCharante <git@jcharante.com>
2017-05-01 15:45:57 -0400

    Spun theGreenAlliance into it's own repository

--- theGreenAlliance/db_functions.py
+++
@@ -1,8 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from db_setup import Base, MatchV1, InviteCodeV1, UserV1, BetV1
-
-engine = create_engine(os.environ['dba'])
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
