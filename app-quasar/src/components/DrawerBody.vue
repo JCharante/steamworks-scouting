20ec0866ddae9184d3e47accbdc18735bd2cbaec
JCharante <git@jcharante.com>
2017-05-21 18:45:05 -0400

    Added Drawer Components

---
+++ app-quasar/src/components/DrawerBody.vue
@@ -0,0 +1,50 @@
+<template>
+    <q-drawer ref="drawer">
+        <div class="toolbar light">
+            <q-toolbar-title :padding="1">
+                Drawer
+            </q-toolbar-title>
+        </div>
+
+        <div class="list no-border platform-delimiter">
+            <q-drawer-link icon="home" to="/" exact>
+                Home
+            </q-drawer-link>
+
+            <hr>
+            <div class="list-label">Scout</div>
+            <q-drawer-link icon="note_add" to="/scout/new_match">
+                New Match
+            </q-drawer-link>
+            <q-drawer-link icon="view_list" to="/">
+                View Matches
+            </q-drawer-link>
+            <q-drawer-link icon="poll" to="/">
+                Overview
+            </q-drawer-link>
+            <q-drawer-link icon="cloud" to="/">
+                Server
+            </q-drawer-link>
+
+            <hr>
+            <div class="list-label">Miscellaneous</div>
+            <q-drawer-link icon="settings" to="/">
+                Settings
+            </q-drawer-link>
+            <q-drawer-link icon="volume_up" to="/">
+                Soundboard
+            </q-drawer-link>
+        </div>
+    </q-drawer>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ app-quasar/src/components/DrawerHead.vue
@@ -0,0 +1,22 @@
+<template>
+    <div slot="header" class="toolbar">
+        <button class="hide-on-drawer-visible" @click="$emit('openDrawerBody')">
+            <i>menu</i>
+        </button>
+        <q-toolbar-title :padding="1">
+            {{ title }}
+        </q-toolbar-title>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['title'],
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+</style>
