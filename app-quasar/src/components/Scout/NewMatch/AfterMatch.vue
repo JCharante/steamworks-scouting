16942c60c4aa898e60223e95460341d9cfbe7887
JCharante <git@jcharante.com>
2017-05-23 04:01:11 -0400

    Added UI for AfterMatch

--- app-quasar/src/components/Scout/NewMatch/AfterMatch.vue
+++ app-quasar/src/components/Scout/NewMatch/AfterMatch.vue
@@ -1,21 +1,112 @@
 <template>
     <div>
         <div class="layout-padding">
-            <p>
-                Layouts are the elements that wrap page content, like navigational bar,
-                drawer, header or footer. Multiple pages can share the same Layout, which
-                is one of the main reason for their existence.
-            </p>
-            <p>
-                You can have multiple headers and/or footers. In each you can place
-                <span class="token">Toolbars</span>,
-                <span class="token">Tabs</span>,
-                <span class="token">Search bars</span>
-                or your own elements.
-            </p>
-            <p>
-                Use it wisely with Vue Router.
-            </p>
+            <div class="list">
+                <div class="list-label">Aftermatch Info</div>
+
+                <label class="item">
+                    <div class="item-primary">
+                        <q-checkbox v-model="match.attemptedClimb"></q-checkbox>
+                    </div>
+                    <div class="item-content">
+                        Attempted to Climb
+                    </div>
+                </label>
+
+                <label class="item">
+                    <div class="item-primary">
+                        <q-checkbox v-model="match.successfullyClimbed"></q-checkbox>
+                    </div>
+                    <div class="item-content">
+                        Successfully Climbed
+                    </div>
+                </label>
+
+                <div class="item two-lines">
+                    <div class="item-content row items-center">
+                        <label style="margin-right: 10px;">Gear Rating:</label>
+                        <br>
+                        <q-select class="full-width" type="radio" v-model="match.gearRating" :options="selectOptions.gearRating"></q-select>
+                    </div>
+                </div>
+
+                <div class="item two-lines">
+                    <div class="item-content row items-center">
+                        <label style="margin-right: 10px;">Low Goal Rating:</label>
+                        <br>
+                        <q-select class="full-width" type="radio" v-model="match.lowGoalRating" :options="selectOptions.lowGoalRating"></q-select>
+                    </div>
+                </div>
+
+                <div class="item two-lines">
+                    <div class="item-content row items-center">
+                        <label style="margin-right: 10px;">High Goal Rating:</label>
+                        <br>
+                        <q-select class="full-width" type="radio" v-model="match.highGoalRating" :options="selectOptions.highGoalRating"></q-select>
+                    </div>
+                </div>
+
+                <div class="item multiple-lines">
+                    <div class="item-content">
+                        <div class="stacked-label" ref="noteDiv">
+                            <textarea class="full-width" :class="{'has-error': match.notes.length > 200}" v-model="match.notes"></textarea>
+                            <label>Notes ({{ 200 - match.notes.length }} chars remaining)</label>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <br>
+
+            <div class="row justify-center">
+                <q-knob size="18rem" v-model="match.estimatedClimbTime" :min="0" :max="30" :placeholder="'Estimated Climb Time: ' + match.estimatedClimbTime + 's'"></q-knob>
+            </div>
+
+
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                match: {
+                    gearDispenseMethod: null,
+                    gearRating: null,
+                    lowGoalRating: null,
+                    highGoalRating: null,
+                    notes: '',
+                    attemptedClimb: false,
+                    successfullyClimbed: false,
+                    estimatedClimbTime: 0
+                },
+                selectOptions: {
+                    gearDispenseMethod: [
+                        {label: 'Non-existent', value: null},
+                        {label: 'Passive', value: 'passive'},
+                        {label: 'Active', value: 'active'}
+                    ],
+                    gearRating: [
+                        {label: 'Bad', value: 'bad'},
+                        {label: 'Okay', value: 'okay'},
+                        {label: 'Good', value: 'good'}
+                    ],
+                    lowGoalRating: [
+                        {label: 'Didn\'t Shoot', value: 'ds'},
+                        {label: 'Bad', value: 'bad'},
+                        {label: 'Okay', value: 'okay'},
+                        {label: 'Good', value: 'good'}
+                    ],
+                    highGoalRating: [
+                        {label: 'Didn\'t Shoot', value: 'ds'},
+                        {label: 'Bad', value: 'bad'},
+                        {label: 'Okay', value: 'okay'},
+                        {label: 'Good', value: 'good'}
+                    ]
+                }
+            }
+        }
+    }
+</script>
