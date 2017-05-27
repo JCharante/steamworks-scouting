9c7e1b7fdafed04c39f92b4a0dcc0c4874eb6a85
JCharante <git@jcharante.com>
2017-05-26 21:08:41 -0400

    Added Drawer Components to new match screen

--- app-quasar/src/components/scout/new.vue
+++ app-quasar/src/components/scout/new.vue
@@ -1,16 +1,8 @@
 <template>
     <q-layout>
-        <!--
-        TODO: Find out why this gives a dozen console errors
         <DrawerHead slot="header" title="Scout New Match" @openDrawerBody="$refs.drawer.$refs.drawer.open()"></DrawerHead>
-        <q-tabs slot="navigation">
-            <q-tab icon="tune" route="/scout/new_match" exact replace>Pre Match</q-tab>
-            <q-tab icon="android" route="/scout/new_match/auto" exact replace>Auto</q-tab>
-            <q-tab icon="face" route="/scout/new_match/teleop" exact replace>Teleop</q-tab>
-            <q-tab icon="gavel" route="/scout/new_match/after_match" exact replace>After Match</q-tab>
-        </q-tabs>
         <DrawerBody ref="drawer"></DrawerBody>
-        -->
+
         <div class="layout-view">
             <div class="layout-padding">
                 <p>Creating New Match</p>
