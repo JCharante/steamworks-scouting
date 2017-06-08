54f472411487a5cc17eddd3e036261297fb02c41
JCharante <git@jcharante.com>
2017-06-07 20:44:41 -0400

    Updated Cordova App Version to 1.9.0.3

--- app-quasar/cordova/buildSigned.sh
+++ app-quasar/cordova/buildSigned.sh
@@ -6,17 +6,22 @@ cordova build --release android
 
 cd platforms/android/build/outputs/apk/
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ksf -storepass $ksp android-x86-release-unsigned.apk $ksa
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ksf -storepass $ksp android-armv7-release-unsigned.apk $ksa
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ksf -storepass $ksp android-release-unsigned.apk $ksa
+#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ksf -storepass $ksp android-x86-release-unsigned.apk $ksa
+#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ksf -storepass $ksp android-armv7-release-unsigned.apk $ksa
 
-touch achilles-$vnum-x86.apk
-touch achilles-$vnum-armv7.apk
+touch achilles-$vnum.apk
+#touch achilles-$vnum-x86.apk
+#touch achilles-$vnum-armv7.apk
 
-mv achilles-$vnum-x86.apk achilles-$vnum-x86.apk.old
-mv achilles-$vnum-armv7.apk achilles-$vnum-armv7.apk.old
+mv achilles-$vnum.apk achilles-$vnum.apk.old
+#mv achilles-$vnum-x86.apk achilles-$vnum-x86.apk.old
+#mv achilles-$vnum-armv7.apk achilles-$vnum-armv7.apk.old
 
-zipalign -v 4 android-x86-release-unsigned.apk achilles-$vnum-x86.apk
-zipalign -v 4 android-armv7-release-unsigned.apk achilles-$vnum-armv7.apk
+zipalign -v 4 android-release-unsigned.apk achilles-$vnum.apk
+#zipalign -v 4 android-x86-release-unsigned.apk achilles-$vnum-x86.apk
+#zipalign -v 4 android-armv7-release-unsigned.apk achilles-$vnum-armv7.apk
 
-mv achilles-$vnum-x86.apk ../../../../../builds/
-mv achilles-$vnum-armv7.apk ../../../../../builds/
+mv achilles-$vnum.apk ../../../../../builds/
+#mv achilles-$vnum-x86.apk ../../../../../builds/
+#mv achilles-$vnum-armv7.apk ../../../../../builds/
