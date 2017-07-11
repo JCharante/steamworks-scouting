4b2f34d4a8167cc74edc6756087c8de81ee12221
JCharante <git@jcharante.com>
2017-07-11 19:33:14 -0400

    Converted Index.vue

---
+++ quasar-app/src/components/Index.vue
@@ -0,0 +1,62 @@
+<template>
+    <q-layout ref="layout">
+        <drawer-head slot="header" title="Home" @toggleLeft="$refs.layout.toggleLeft()"></drawer-head>
+        <drawer-body slot="left"></drawer-body>
+        <div class="layout-view">
+            <div class="layout-padding">
+                <h4 class="text-center">Steamworks Scouting</h4>
+                <p class="text-center">by the Innocuous Achilles group</p>
+            </div>
+        </div>
+
+    </q-layout>
+</template>
+
+<script>
+    import {
+        dom,
+        event,
+        openURL,
+        QLayout,
+        QField,
+        QInput,
+        QToolbar,
+        QToolbarTitle,
+        QBtn,
+        QIcon,
+        QList,
+        QListHeader,
+        QItem,
+        QItemSide,
+        QItemMain,
+        QKnob
+    } from 'quasar'
+    import DrawerHead from './Drawer/Head.vue'
+    import DrawerBody from './Drawer/Body.vue'
+
+    export default {
+        components: {
+            dom,
+            event,
+            openURL,
+            QLayout,
+            QField,
+            QInput,
+            QToolbar,
+            QToolbarTitle,
+            QBtn,
+            QIcon,
+            QList,
+            QListHeader,
+            QItem,
+            QItemSide,
+            QItemMain,
+            QKnob,
+            DrawerHead,
+            DrawerBody
+        },
+        data () {
+            return {}
+        }
+    }
+</script>
