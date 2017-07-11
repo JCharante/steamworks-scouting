37cc23a95ee563034395300ec111346c225ab5fb
JCharante <git@jcharante.com>
2017-07-11 19:32:11 -0400

    Converted Drawers

---
+++ quasar-app/src/components/Drawer/Body.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <q-list no-border link inset-delimiter>
+            <q-list-header>Steamworks Scouting</q-list-header>
+            <q-side-link item to="/">
+                <q-item-side icon="home" />
+                <q-item-main label="Home"/>
+            </q-side-link>
+
+            <q-list-header>Scout</q-list-header>
+            <q-side-link item to="/scout/new">
+                <q-item-side icon="note_add" />
+                <q-item-main label="New Match"/>
+            </q-side-link>
+            <q-side-link item to="/scout/view">
+                <q-item-side icon="view_list" />
+                <q-item-main label="View Matches"/>
+            </q-side-link>
+            <q-side-link item to="/">
+                <q-item-side icon="poll" />
+                <q-item-main label="Overview"/>
+            </q-side-link>
+            <q-side-link item to="/server">
+                <q-item-side icon="cloud" />
+                <q-item-main label="Server"/>
+            </q-side-link>
+
+            <q-list-header>Miscellaneous</q-list-header>
+            <q-side-link item to="/settings">
+                <q-item-side icon="settings" />
+                <q-item-main label="Settings"/>
+            </q-side-link>
+            <q-side-link item to="/soundboard">
+                <q-item-side icon="volume_up" />
+                <q-item-main label="Soundboard"/>
+            </q-side-link>
+        </q-list>
+    </div>
+</template>
+
+<script>
+    import {
+        dom,
+        event,
+        openURL,
+        QLayout,
+        QToolbar,
+        QToolbarTitle,
+        QBtn,
+        QIcon,
+        QList,
+        QListHeader,
+        QItem,
+        QItemSide,
+        QItemMain,
+        QSideLink
+    } from 'quasar'
+    export default {
+        components: {
+            dom,
+            event,
+            openURL,
+            QLayout,
+            QToolbar,
+            QToolbarTitle,
+            QBtn,
+            QIcon,
+            QList,
+            QListHeader,
+            QItem,
+            QItemSide,
+            QItemMain,
+            QSideLink
+        },
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ quasar-app/src/components/Drawer/Head.vue
@@ -0,0 +1,67 @@
+<template>
+    <!--
+    <div slot="header" class="toolbar">
+        <button v-if="backUrl" class="hide-on-drawer-visible" @click="$router.push(backUrl)">
+            <i>keyboard_arrow_left</i>
+        </button>
+        <button v-else class="hide-on-drawer-visible" @click="$emit('openDrawerBody')">
+            <i>menu</i>
+        </button>
+        <q-toolbar-title :padding="1">
+            {{ title }}
+        </q-toolbar-title>
+    </div>
+    -->
+    <q-toolbar>
+        <q-btn flat @click="$emit('toggleLeft')">
+            <q-icon name="menu"></q-icon>
+        </q-btn>
+
+        <q-toolbar-title>
+            {{ title }}
+            <div v-if="subtitle" slot="subtitle">{{ subtitle }}</div>
+        </q-toolbar-title>
+    </q-toolbar>
+</template>
+
+<script>
+    import {
+        dom,
+        event,
+        openURL,
+        QLayout,
+        QToolbar,
+        QToolbarTitle,
+        QBtn,
+        QIcon,
+        QList,
+        QListHeader,
+        QItem,
+        QItemSide,
+        QItemMain
+    } from 'quasar'
+    export default {
+        components: {
+            dom,
+            event,
+            openURL,
+            QLayout,
+            QToolbar,
+            QToolbarTitle,
+            QBtn,
+            QIcon,
+            QList,
+            QListHeader,
+            QItem,
+            QItemSide,
+            QItemMain
+        },
+        props: ['title', 'backUrl', 'subtitle'],
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style>
+</style>
