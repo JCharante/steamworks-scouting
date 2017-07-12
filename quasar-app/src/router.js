a077ec8a2206d9a2a1feaa44bafbae2b8887bf74
JCharante <git@jcharante.com>
2017-07-11 20:12:03 -0400

    Added quasar js files

---
+++ quasar-app/src/router.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+function load (component) {
+    // '@' is aliased to src/components
+    return () => System.import(`@/${component}.vue`)
+}
+
+export default new VueRouter({
+    /*
+     * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
+     * it is only to be used only for websites.
+     *
+     * If you decide to go with "history" mode, please also open /config/index.js
+     * and set "build.publicPath" to something other than an empty string.
+     * Example: '/' instead of current ''
+     *
+     * If switching back to default "hash" mode, don't forget to set the
+     * build publicPath back to '' so Cordova builds work again.
+     */
+
+    routes: [
+        { path: '/settings', component: load('Settings') },
+        { path: '/server',
+            component: load('server/layout'),
+            children: [
+                { path: '', component: load('server/Upload') },
+                { path: 'download', component: load('server/Download') }
+            ]
+        },
+        { path: '/scout/view',
+            component: load('scoutedMatches/layout'),
+            children: [
+                { path: '', component: load('scoutedMatches/PickEvent') },
+                { path: 'event/:eventCode', component: load('scoutedMatches/PickMatch') },
+                { path: 'event/:eventCode/match/:matchNumber', component: load('scoutedMatches/PickTeam') }
+            ]
+        },
+        { path: '/scout/new', component: load('scout/NewMatch') },
+        { path: '/scout/edit/:matchID',
+            component: load('scout/layout'),
+            children: [
+                { path: '', component: load('scout/PreMatch') },
+                { path: 'auto', component: load('scout/Auto') },
+                { path: 'teleop', component: load('scout/Teleop') },
+                { path: 'after_match', component: load('scout/AfterMatch') }
+            ]
+        },
+        { path: '/demo',
+            component: load('demo/layout'),
+            children: [
+                { path: '', component: load('demo/TabOne') },
+                { path: 'tabtwo', component: load('demo/TabTwo') }
+            ]
+        },
+        { path: '/soundboard', component: load('SoundBoard') },
+        { path: '/', component: load('Index') }, // Default
+        { path: '*', component: load('Error404') } // Not found
+    ]
+})
